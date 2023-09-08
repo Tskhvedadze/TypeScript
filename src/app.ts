c846f6a86cd5a424853be39fb7f23ea2192d3259
Tskhvedadze <giorgicxvedadze4@gmail.com>
2023-09-08 10:53:36 +0400

    app ts project

--- src/app.ts
+++ src/app.ts
@@ -1,101 +1,66 @@
-/*
+//autobind decorator
+function Autobind(_: any, __: string, descriptor: PropertyDescriptor) {
+  const originalMethod = descriptor.value;
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+}
 
-const userName1 = "Max";
+// Project Class
+class ProjectInput {
+  templateElement: HTMLTemplateElement;
+  hostElement: HTMLDivElement;
+  element: HTMLFormElement;
+  titleInputElement: HTMLInputElement;
+  descriptionInputElement: HTMLInputElement;
+  peopleInputElement: HTMLInputElement;
 
-let age = 29;
-age = 30;
+  constructor() {
+    this.templateElement = <HTMLTemplateElement>(
+      document.getElementById("project-input")!
+    );
+    this.hostElement = <HTMLDivElement>document.getElementById("app")!;
 
-const add2 = (n1: number, n2: number = 23): number => n1 + n2;
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+    this.element = <HTMLFormElement>importedNode.firstElementChild;
+    this.element.id = "user-input";
 
-console.log(add2(23));
+    this.titleInputElement = <HTMLInputElement>(
+      this.element.querySelector("#title")
+    );
+    this.descriptionInputElement = <HTMLInputElement>(
+      this.element.querySelector("#description")
+    );
+    this.peopleInputElement = <HTMLInputElement>(
+      this.element.querySelector("#people")
+    );
 
-const printOutput = (output: string | number) => console.log(output);
-
-printOutput(add2(34));
-
-const hobbies = ["sport", "coocikng"];
-const [hobbie1, hobbie2] = hobbies;
-console.log(hobbie1, hobbie2);
-
-const activeHobbies = ["Hiking"];
-
-activeHobbies.push(...hobbies);
-
-console.log(activeHobbies);
-
-const person3 = {
-  name2: "Max",
-  age2: 40,
-};
-
-const { name2, age2 } = person3;
-
-const copiedPerson3 = { ...person3 };
-
-copiedPerson3.age2 = 50;
-
-console.log(person3);
-
-const adds = (...numbers: [number, number, number]) => {
-  return numbers.reduce((a, b) => a + b, 0);
-};
-
-console.log(adds(1, 2, 4));
-
-let A: number = 10;
-let B: number = 20;
-
-console.log(A, B);
-
-[B, A] = [A, B];
-
-console.log(A, B);
-
-
-// Classes
-
-class Department {
-  // public name: string;
-  private employees: string[] = [];
-
-  constructor(public readonly name: string) {
-    // this.name = n;
+    this.attach();
+    this.configure();
   }
 
-  describe(this: Department) {
-    console.log(`Department: ${this.name}`);
+  @Autobind
+  private submitHandler(e: Event) {
+    e.preventDefault();
+
+    console.log(this.titleInputElement.value);
   }
 
-  addEmployes(employe: string) {
-    this.employees.push(employe);
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler);
   }
 
-  printEmployeeInformation() {
-    console.log(this.employees.length);
-    console.log(this.employees);
+  private attach() {
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
 }
 
-class ITDepartment extends Department {
-  admins: string[];
-  private lastReport: string;
-  constructor(name: string, admins: string[]) {
-    super(name);
-    this.admins = admins;
-    this.lastReport = "report";
-  }
-}
-
-const accounting = new ITDepartment("Accounting", ["Max"]);
-accounting.addEmployes("Max");
-accounting.addEmployes("Manu");
-console.log(accounting);
-
-accounting.describe();
-accounting.printEmployeeInformation();
-
-// const accountingCopy = { name: "DUMMY", describe: accounting.describe };
-
-// accountingCopy.describe();
-
-*/
+const projectInput = new ProjectInput();
